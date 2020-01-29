6576899e8ee24525e4efcb811faf369745b3c4a2
AloneUSB <60081766+AloneUSB@users.noreply.github.com>
2020-01-29 17:28:13 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"Based Pack"
-    packshortname = f"Based Pack_Crusader{userid}"  # format: Uni_Borg_userid
+    packshortname = f"BasedQuotes{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "r_Sticker.png"
